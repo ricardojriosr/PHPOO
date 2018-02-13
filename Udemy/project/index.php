9d606d0037bc9f0623cb7166256f4569de898e81
Ricardo Rios <ricardojriosr@gmail.com>
2018-02-13 12:30:31 -0500

    Database __construct

---
+++ Udemy/project/index.php
@@ -0,0 +1,19 @@
+<?php
+require 'classes/Database.php';
+
+$database = new Database;
+
+$database->query('SELECT * FROM posts WHERE id = :id');
+$database->bind(':id', 1);
+$rows = $database->resultset();
+//print_r($rows);
+?>
+<h1>Posts</h1>
+<div>
+<?php foreach ($rows as $row) : ?>
+    <div>
+        <h3><?php echo $row['title']; ?></h3>
+        <p><?php echo $row['body']; ?></p>
+    </div>
+<?php endforeach; ?>
+</div>
