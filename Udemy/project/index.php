382f93acf8fcb6b75e7f32cb88a46a78051d755d
Ricardo Rios <ricardojriosr@gmail.com>
2018-02-14 14:28:28 -0500

    Adding Posts

--- Udemy/project/index.php
+++ Udemy/project/index.php
@@ -3,11 +3,41 @@ require 'classes/Database.php';
 
 $database = new Database;
 
-$database->query('SELECT * FROM posts WHERE id = :id');
-$database->bind(':id', 1);
+
+
+$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+if ($post['submit']) {
+    $title = $post['title'];
+    $body = $post['body'];
+
+    $database->query('INSERT INTO posts (title,body) VALUES (:title, :body)');
+    $database->bind(':title',$title);
+    $database->bind(':body',$body);
+    $database->execute();
+    if ($database->lastInsertId()) {
+        echo '<p>Post Added</p>';
+    }
+}
+
+$database->query('SELECT * FROM posts');
+
 $rows = $database->resultset();
 //print_r($rows);
+
 ?>
+
+<h1>Add Post</h1>
+<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+  <label>Post Title</label><br>
+  <input type="text" name="title" placeholder="Add a Title..." />
+  <br><br>
+  <label>Post Body</label><br>
+  <textarea name="body"></textarea>
+  <br><br>
+  <input type="submit" name="submit" value="submit" />
+</form>
+
 <h1>Posts</h1>
 <div>
 <?php foreach ($rows as $row) : ?>
