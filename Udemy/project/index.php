25509861a9b6db7e5d3ad0c84f1c41640ce7c087
Ricardo Rios <ricardojriosr@gmail.com>
2018-02-15 12:36:22 -0500

    CRUD

--- Udemy/project/index.php
+++ Udemy/project/index.php
@@ -6,17 +6,32 @@ $database = new Database;
 
 
 $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-if ($post['submit']) {
+if (isset($_POST['delete'])) {
+    $delete_id = $_POST['delete_id'];
+    $database->query('DELETE FROM posts WHERE id = :id');
+    $database->bind(':id',$delete_id);
+    $database->execute();
+    echo '<p>Post Deleted</p>';
+}
+if (isset($post['submit'])) {
+    $id = $post['id'];
     $title = $post['title'];
     $body = $post['body'];
-
-    $database->query('INSERT INTO posts (title,body) VALUES (:title, :body)');
-    $database->bind(':title',$title);
-    $database->bind(':body',$body);
-    $database->execute();
-    if ($database->lastInsertId()) {
-        echo '<p>Post Added</p>';
+    if ($id == "") {
+        $database->query('INSERT INTO posts (title,body) VALUES (:title, :body)');
+        $database->bind(':title',$title);
+        $database->bind(':body',$body);
+        $database->execute();
+        if ($database->lastInsertId()) {
+            echo '<p>Post Added</p>';
+        }
+    } else {
+        $database->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+        $database->bind(':title',$title);
+        $database->bind(':body',$body);
+        $database->bind(':id',$id);
+        $database->execute();
+        echo '<p>Post Updated</p>';
     }
 }
 
@@ -29,6 +44,9 @@ $rows = $database->resultset();
 
 <h1>Add Post</h1>
 <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+  <label>Post ID</label><br>
+  <input type="text" name="id" placeholder="Specify ID" />
+  <br><br>
   <label>Post Title</label><br>
   <input type="text" name="title" placeholder="Add a Title..." />
   <br><br>
@@ -44,6 +62,11 @@ $rows = $database->resultset();
     <div>
         <h3><?php echo $row['title']; ?></h3>
         <p><?php echo $row['body']; ?></p>
+        <br/>
+        <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+            <input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>" />
+            <input type="submit" name="delete" value="Delete" />
+        </form>
     </div>
 <?php endforeach; ?>
 </div>
